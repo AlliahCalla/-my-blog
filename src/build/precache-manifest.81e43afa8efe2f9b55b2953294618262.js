32bb2c8504ed95a934d388bc82f06a835e4b87b1
Alliah <alliahczarielle17@gmail.com>
2020-09-01 16:02:46 +1200

    First Commit

---
+++ src/build/precache-manifest.81e43afa8efe2f9b55b2953294618262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9243ca0dba3b1cc4e89f324f0007cc6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2261774f147c21e30cc5",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9f27527568c48733aa8c",
+    "url": "/static/js/2.ca0bc356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca0bc356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2261774f147c21e30cc5",
+    "url": "/static/js/main.fa98d16b.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
